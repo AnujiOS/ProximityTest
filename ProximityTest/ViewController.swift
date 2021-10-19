acf6e753411e921ac4a093c5e9fd5da95416f647
anujjoshi <anuj@groktor.net>
2021-10-19 14:43:10 +0530

    Initial Commit

---
+++ ProximityTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProximityTest
+//
+//  Created by Anuj Joshi on 19/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
