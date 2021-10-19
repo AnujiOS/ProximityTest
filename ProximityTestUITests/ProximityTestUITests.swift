2426ee91726920dae1111eb1d33b3b5606433eab
anujjoshi <anuj@groktor.net>
2021-10-20 00:06:53 +0530

    UI Test cases added

--- ProximityTestUITests/ProximityTestUITests.swift
+++ ProximityTestUITests/ProximityTestUITests.swift
@@ -9,26 +9,50 @@ import XCTest
 
 class ProximityTestUITests: XCTestCase {
 
+    var app: XCUIApplication!
+
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        app = XCUIApplication()
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
+    func testTableInteraction() {
 
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Assert that we are displaying the tableview
+        let cityTableView = app.tables["table--cityTableView"]
+        XCTAssertTrue(cityTableView.exists, "The city list tableview exists")
+
+        // Get an array of cells
+        let tableCells = cityTableView.cells
+
+        if tableCells.count > 0 {
+            let count: Int = (tableCells.count - 1)
+
+            let promise = expectation(description: "Wait for table cells")
+
+            for i in stride(from: 0, to: count , by: 1) {
+                // Grab the first cell and verify that it exists and tap it
+                let tableCell = tableCells.element(boundBy: i)
+                XCTAssertTrue(tableCell.exists, "The \(i) cell is in place on the table")
+                // Does this actually take us to the next screen
+                tableCell.tap()
+
+                if i == (count - 1) {
+                    promise.fulfill()
+                }
+                // Back
+                app.navigationBars.buttons.element(boundBy: 0).tap()
+            }
+            waitForExpectations(timeout: 20, handler: nil)
+            XCTAssertTrue(true, "Finished validating the table cells")
+
+        } else {
+            XCTAssert(false, "Was not able to find any table cells")
+        }
     }
 
     func testLaunchPerformance() throws {
